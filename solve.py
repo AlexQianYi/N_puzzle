66455a0ac342feeddea8e1a4da5338e8013d8bf4
AlexQianYi <q563875735@gmail.com>
2018-04-04 15:56:49 -0400

    table

--- solve.py
+++ solve.py
@@ -9,13 +9,14 @@ Created on Wed Apr  4 12:02:22 2018
 import utility
 import copy
 import environment
+import table
 
 
 class solve:
     
     def __init__(self, input_table):
         
-        self.initial_state = copy.deepcopy(self,list_to_table(input_table))       
+        self.initial_state = copy.deepcopy(input_table)       
         self.goal_state = self.set_goal_state(input_table)      
         self.AStarQueue = utility.PriorityQueue()
         
@@ -49,23 +50,52 @@ class solve:
             
         print("Error")
         
-        def ExpandNode(self, table):
+    def ExpandNode(self, table):
             
-            action = ['u', 'd', 'l', 'r']
+        action = ['u', 'd', 'l', 'r']
             
-            for act in action:
+        for act in action:
                 
-                temp_table = table.table(table.state)
+            temp_table = table.table(table.state)
                 
-                temp_table.path_hisroty = copy.copy(table.path_history)
+            temp_table.path_hisroty = copy.copy(table.path_history)
                 
-                if temp_table.move(node):
-                    temp_table.path_history.append(node)
+            if temp_table.move(node):
+                temp_table.path_history.append(node)
                     
-                    if temp_table not in self.froniter and temp_table not in self.visited:
-                        temp_table.score = temp_table.manhattan(self.goal_state)
-                        self.AStarQueue.queue.put((temp_table.score, temp_table))
+                if temp_table not in self.froniter and temp_table not in self.visited:
+                    temp_table.score = temp_table.manhattan(self.goal_state)
+                    self.AStarQueue.queue.put((temp_table.score, temp_table))
                         
-                    self.env.UpdateQueue()
+                self.env.UpdateQueue()
                     
-                self.env.node_expanded +=1
+            self.env.node_expanded +=1
+            
+    def GoalTest(self, state):
+        if state.state == self.goal_state:
+            return True
+        else:
+            return False
+        
+    def SetGoalState(self, input_list):
+        
+        n = int(len(input_list))
+        goal_state = [['-' for x in range(n)] for y in range(n)]
+        
+        i,j = 0, 0
+        count = 1
+        
+        while i<n:
+            if count == n*n:
+                count=0
+            goal_state[i][j] =count
+            count+=1
+            j +=1
+            if j==n:
+                j=0
+                i+=1
+        return goal_state
+    
+    
+                
+                
--- table.py
+++ table.py
@@ -6,6 +6,7 @@ Created on Wed Apr  4 10:18:18 2018
 @author: yiqian
 """
 import copy
+import math
 
 class table:
     
@@ -16,31 +17,54 @@ class table:
         self.score = 0
         self.n = N
         
-    def blank(self, table):
+    def find(self, tile, table):
         blank_x, blank_y = 0,0
         for i in range(self.n):
             for j in range(self.n):
-                if table[i][j]==0:
+                if table[i][j]==tile:
                     blank_x, blank_y=i, j
         return blank_x, blank_y
         
     def move(self, action):
         print('move')
-        blank_x, blank_y = self.blank(self.table)
+        blank_x, blank_y = self.find(0, self.table)
     
         if action=='u':
-            self.table[blank_x][blank_y]=self.table[blank_x+1][blank_y]
-            self.table[blank_x+1][blank_y]=0
-            return temp_table
+            y,x = 1,0
         if action=='d':
-            temp_table[blank_x][blank_y]=temp_table[blank_x-1][blank_y]
-            temp_table[blank_x-1][blank_y]=0
-            return temp_table
+            y,x = -1,0
         if action=='l':
-            temp_table[blank_x][blank_y]=temp_table[blank_x][blank_y+1]
-            temp_table[blank_x][blank_y+1]=0
-            return temp_table
+            y,x = 0,1
         if action=='r':
-            temp_table[blank_x][blank_y]=temp_table[blank_x][blank_y-1]
-            temp_table[blank_x][blank_y-1]=0
-            return temp_table
+            y,x = 0,-1
+        
+        if blank_y+y not in range(0, self.n):
+            return False
+        if blank_x+x not in range(0, self.n):
+            return False
+        
+        temp_table = self.table[blank_x+x][blank_y+y]
+        self.table[blank_x][blank_y] = temp_table
+        self.table[blank_x+x][blank_y+y]=0
+        
+        return True
+    
+    def Manhattan(self, goal_state):
+        
+        sum = 0
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.state[i][j]==0:
+                    continue
+                else:
+                    sum += self.ManhattanDis(self.state[i][j], (i,j), goal_state)
+                    
+        return sum
+    
+    def ManhattanDis(self, tile, position, goal_state):
+        
+        blank_x, blank_y = self.find(tile, goal_state)
+        
+        distance = (abs(position[0]-blank_x) + abs(position[1]-blank_y))
+        
+        return distance
